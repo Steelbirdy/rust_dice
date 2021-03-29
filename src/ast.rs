7f1ed972f9f45833b2cfe1bffacacda2e1560c5d
Steelbirdy <patrickammons@comcast.net>
2021-03-28 21:24:18 -0400

    Removed unused InnerNode impl

--- src/ast.rs
+++ src/ast.rs
@@ -32,7 +32,10 @@ pub enum Op {
 
 impl Node {
     fn new(op: Op, left: Option<Node>, right: Option<Node>) -> Self {
-        Node::Node(InnerNode::new(op, left, right))
+        let left = if let Some(inner) = left { Some(Box::new(inner)) } else { None };
+        let right = if let Some(inner) = right { Some(Box::new(inner)) } else { None };
+
+        Node::Node(InnerNode { op, left, right })
     }
 
     #[allow(non_snake_case)]
@@ -77,50 +80,5 @@ impl Node {
     #[allow(non_snake_case)]
     pub fn Dice(num: i32, sides: i32) -> Node {
         Node::new(Op::Dice { num, sides }, None, None)
-    }
-}
-
-
-impl InnerNode {
-    fn new(op: Op, left: Option<Node>, right: Option<Node>) -> Self {
-        let left = if let Some(inner) = left { Some(Box::new(inner)) } else { None };
-        let right = if let Some(inner) = right { Some(Box::new(inner)) } else { None };
-
-        InnerNode { op, left, right }
-    }
-
-    #[allow(non_snake_case)]
-    pub fn Add(left: Node, right: Node) -> InnerNode {
-        InnerNode::new(Op::Add, Some(left), Some(right))
-    }
-
-    #[allow(non_snake_case)]
-    pub fn Sub(left: Node, right: Node) -> InnerNode {
-        InnerNode::new(Op::Sub, Some(left), Some(right))
-    }
-
-    #[allow(non_snake_case)]
-    pub fn Mul(left: Node, right: Node) -> InnerNode {
-        InnerNode::new(Op::Mul, Some(left), Some(right))
-    }
-
-    #[allow(non_snake_case)]
-    pub fn Div(left: Node, right: Node) -> InnerNode {
-        InnerNode::new(Op::Div, Some(left), Some(right))
-    }
-
-    #[allow(non_snake_case)]
-    pub fn Parens(inner: Node) -> InnerNode {
-        InnerNode::new(Op::Parens, Some(inner), None)
-    }
-
-    #[allow(non_snake_case)]
-    pub fn Number(value: i32) -> InnerNode {
-        InnerNode::new(Op::Number(value), None, None)
-    }
-
-    #[allow(non_snake_case)]
-    pub fn Dice(num: i32, sides: i32) -> InnerNode {
-        InnerNode::new(Op::Dice { num, sides }, None, None)
     }
 }
